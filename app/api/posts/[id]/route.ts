b85d343b5dd1fc572daf30242a7891eacee9c4f6
Jad Kobeissi <stapler12344321@gmail.com>
2025-03-27 15:45:18 +0200

    test

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,33 +1,14 @@
-import { verify } from "jsonwebtoken";
-import { prisma } from "../../init";
+import { verify } from "jsonwebtoken"
 
-export async function GET(req: Request, { context }: { context: { params: Promise<{id: number}> }}) {
-    try {{}
-        const authHeader = req.headers.get("Authorization")?.split(' ')[1];
-        
-        if (!authHeader || !verify(authHeader, process.env.SECRET as string)) {
-            return new Response("Unauthorized", { status: 401 });
-        }
+export async function GET(req: Request, { params }: { params: Promise<{id: Number}>}) {
+    try {
+        const authHeader = req.headers.get("Authorization")?.split(' ')[1]
 
-        const { id } = await context.params;
+        if (!authHeader || !verify(authHeader, process.env.SECRET as string)) return new Response("Unauthorized", { status: 401 })
 
-        const post = await prisma.post.findUnique({
-            where: {
-                id: Number(id)
-            },
-            include: {
-                author: true
-            }
-        });
-
-        if (!post) {
-            return new Response("Post not found", { status: 404 });
-        }
-
-        return new Response(JSON.stringify(post), {
-            headers: { "Content-Type": "application/json" }
-        });
+        const { id } = await params
+        return new Response(id as any)
     } catch (error: any) {
-        return new Response(error, { status: 500 });
+        return new Response(error, { status: 500 })
     }
 }
