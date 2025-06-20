1c9528ebf30bef3594788bdcfee76d440c87bc14
Jad kobeissi <jadkoneissi@gmail.com>
2025-06-20 13:47:01 +0300

    Added user viewing their own posts backend

---
+++ app/api/posts/user/[page]/route.ts
@@ -0,0 +1,38 @@
+import { decode, verify } from "jsonwebtoken"
+import { prisma } from "@/app/api/prisma"
+
+export async function GET(req: Request, { params }: { params: { page: number } }) {
+    try {
+
+        const authHeader = req.headers.get("Authorization")?.split(' ')[1]
+
+        if (!authHeader || !verify(authHeader, process.env.SECRET as string)) return new Response("Unauthorized", { status: 401 })
+
+        const decoded: any = await decode(authHeader)
+        const page = params.page ? params.page : 1
+        const skip = (page - 1) * 5;
+        const posts = await prisma.post.findMany({
+            where: {
+                authorId: {
+                        equals: decoded.id
+                }
+            },
+            include: {
+                author: true,
+                viewedUsers: true
+            },
+            take: 5,
+            skip: skip,
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+
+        if (posts.length == 0) return new Response("Posts not found", { status: 404 })
+
+
+        return Response.json(posts)
+    } catch (error: any) {
+        return new Response(error, { status: 500 })
+    }
+}
