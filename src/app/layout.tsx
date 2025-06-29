694beae83ba3b88b324bdbbc8b7dad25c363c2f1
Jad Kobeissi <jadkoneissi@gmail.com>
2025-06-29 20:31:18 +0300

    feat: add Google Ads script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3734297456127533"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        ></Script>
         {children}
       </body>
     </html>
